f6b505362152b2a993e36e50d93506fd88f48dd1
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-20 20:30:15 +0200

    adding additional thread

--- src/main/java/dev/morling/onebrc/TestThreadImplementationDump.java
+++ src/main/java/dev/morling/onebrc/TestThreadImplementationDump.java
@@ -2,18 +2,24 @@ package dev.morling.onebrc;
 
 import java.io.IOException;
 
-import static java.lang.Thread.sleep;
-
 public class TestThreadImplementationDump implements Runnable {
     public static void main(String[] args) throws IOException {
-        Thread thread = new Thread(new TestThreadImplementationDump());
-        thread.start();
+        Thread thread1 = new Thread(new TestThreadImplementationDump());
+        Thread thread2 = new Thread(new TestThreadImplementationDump());
+
+        thread1.start();
+        thread2.start();
     }
 
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("I am thread number: " + i);
+            System.out.println("I am thread number: " + i + " " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
