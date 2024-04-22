54ab863a390b59d745527856ea2e741959802d68
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-22 22:49:00 +0200

    add executor service

--- src/main/java/dev/morling/onebrc/TestThreadImplementationRunnable.java
+++ src/main/java/dev/morling/onebrc/TestThreadImplementationRunnable.java
@@ -1,14 +1,28 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TestThreadImplementationRunnable implements Runnable {
     public static void main(String[] args) throws IOException {
-        Thread thread1 = new Thread(new TestThreadImplementationRunnable());
-        Thread thread2 = new Thread(new TestThreadImplementationRunnable());
 
-        thread1.start();
-        thread2.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        executorService.execute(new TestThreadImplementationRunnable());//these are carried out in order they're added
+        // to executor service, but guarantee goes when using more threads
+        executorService.execute(() -> System.out.println("I'M A RANDOM TASK"));
+
+        System.out.println("This is another RANDOM TASK!");//this is out of sequence -- separate thread
+
+        executorService.shutdown();
+
+//        Thread thread1 = new Thread(new TestThreadImplementationRunnable());
+//        Thread thread2 = new Thread(new TestThreadImplementationRunnable());
+
+//        thread1.start();
+//        thread2.start();
     }
 
     @Override
