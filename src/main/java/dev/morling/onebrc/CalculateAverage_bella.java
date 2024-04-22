f1679b16d8fd42cdeaec9fe704bad1c0a60dcf53
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-23 00:07:23 +0200

    executor calculate average wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_bella.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_bella.java
@@ -1,16 +1,54 @@
 package dev.morling.onebrc;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collector;
 
 import static java.util.stream.Collectors.groupingBy;
 
-public class CalculateAverage_bella {
+public class CalculateAverage_bella implements Runnable {
     private static final String FILE = "./measurements.txt";
+    private static String line = null;
+
+    Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
+            MeasurementAggregator::new,
+            (a, m) -> {
+                a.min = Math.min(a.min, m.value);
+                a.max = Math.max(a.max, m.value);
+                a.sum += m.value;
+                a.count++;
+            },
+            (agg1, agg2) -> {
+                var res = new MeasurementAggregator();
+                res.min = Math.min(agg1.min, agg2.min);
+                res.max = Math.max(agg1.max, agg2.max);
+                res.sum = agg1.sum + agg2.sum;
+                res.count = agg1.count + agg2.count;
+
+                return res;
+            },
+            agg -> {
+                return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
+            });
+
+    @Override
+    public void run() {
+        String line = getLine();
+        System.out.println(line);
+//        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
+//        .map(l -> new Measurement(l.split(";")))
+//        .collect(groupingBy(m -> m.station(), collector)));
+    }
 
     private static class MeasurementAggregator {
         private double min = Double.POSITIVE_INFINITY;
@@ -42,6 +80,14 @@ public class CalculateAverage_bella {
         }
     };
 
+    private static void setLine(String line) {
+        CalculateAverage_bella.line = line;
+    }
+
+    private static String getLine() {
+        return CalculateAverage_bella.line;
+    }
+
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
 
@@ -54,32 +100,27 @@ public class CalculateAverage_bella {
         // .collect(toMap(e -> e.getKey(), e -> Math.round(e.getValue() * 10.0) / 10.0)));
         // System.out.println(measurements1);
 
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
+        try(ExecutorService executorService = Executors.newSingleThreadExecutor()) {
 
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
+            try(var file = new FileInputStream(FILE)) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+                reader.lines().forEach(line -> {
+                    setLine(line);
+                    executorService.submit(new CalculateAverage_bella());//these are carried out in order they're added
                 });
+            }
 
-        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
-                .map(l -> new Measurement(l.split(";")))
-                .collect(groupingBy(m -> m.station(), collector)));
+            executorService.shutdown();
+            try {
+                if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                    executorService.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executorService.shutdownNow();
+            }
+        }
 
-        System.out.println(measurements);
+//        System.out.println(measurements);
         System.out.printf("Task finished in %s ms%n", System.currentTimeMillis() - start);
     }
 }
