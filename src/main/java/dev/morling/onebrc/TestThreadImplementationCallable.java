8c7410037c4b5a2abe3121f5f79a30416b288c4d
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-22 23:08:01 +0200

    add Future example

--- src/main/java/dev/morling/onebrc/TestThreadImplementationCallable.java
+++ src/main/java/dev/morling/onebrc/TestThreadImplementationCallable.java
@@ -1,16 +1,36 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class TestThreadImplementationCallable implements Callable<String> {
     /*Instead of overriding run() method, for this one we override call() method,
     which unlike run() can throw an exception and can return a generic value*/
 
     public static void main(String[] args) throws Exception {
-        TestThreadImplementationCallable callable = new TestThreadImplementationCallable();
-        String result = callable.call();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> future = executorService.submit(new TestThreadImplementationCallable());
+        //.submit and .invokeAll methods return Futures
+
+        String result = null;
+        try {
+            result = future.get(200, TimeUnit.MILLISECONDS); //Calling the get() method while the task is still
+            // running will cause execution to block until the task properly executes and the result is available.
+            //adding a timeout can prevent degradation
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(result);
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
     }
 
         @Override
