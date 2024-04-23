e3a8a88c413e88d313bb108faa05ce3542b57c63
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-04-24 00:15:57 +0200

    add thread for calculating

--- src/main/java/dev/morling/onebrc/CalculateAverage_bella.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_bella.java
@@ -17,16 +17,19 @@ public class CalculateAverage_bella {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         long start = System.currentTimeMillis(); //timer start
 
-        Map<String, Double> parsedLines = null;
+        Map<String, Double> measurements;
 
         try(ExecutorService executorService = Executors.newSingleThreadExecutor()) {
             try (Stream<String> streamedFile = Files.lines(Paths.get(FILE))) {
                 Future<Map<String, Double>> future = executorService.submit(new ParseLines(streamedFile));
                 while(!future.isDone()) {
-                    System.out.println("Waiting for future...");
-                    Thread.sleep(1000);
+                    System.out.printf("Waiting for future... System time now: %d %n", System.currentTimeMillis());
+                    Thread.sleep(5000);
                 }
-                parsedLines = future.get();
+                measurements = future.get();
+                System.out.println("Now calculating averages");
+                executorService.execute(new CalculateLines(measurements));
+
                 executorService.shutdown();
                 try {
                     if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
@@ -38,19 +41,8 @@ public class CalculateAverage_bella {
             }
         }
 
-        Map<String, Double> measurements = new TreeMap<>(parsedLines.entrySet()
-                .stream()
-                .collect(toMap(Map.Entry::getKey, e -> Math.round(e.getValue() * 10.0) / 10.0)));
-
         System.out.println(measurements);
         System.out.printf("Task finished in %s ms%n", System.currentTimeMillis() - start);
-    }
-
-
-    private record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
     }
 
     private static class ParseLines implements Callable<Map<String, Double>> {
@@ -64,6 +56,22 @@ public class CalculateAverage_bella {
         public Map<String, Double> call() throws Exception {
             return stream.map(line -> line.split(";"))
                     .collect(groupingBy(metric -> metric[0], averagingDouble(metric -> Double.parseDouble(metric[1]))));
+        }
+    }
+
+    private static class CalculateLines implements Runnable {
+        Map<String, Double> measurements;
+
+        public CalculateLines(Map<String, Double> parsedLines) {
+            measurements = parsedLines;
+        }
+
+        @Override
+        public void run() {
+            new TreeMap<>(measurements.entrySet()
+                    .stream()
+                    .collect(toMap(Map.Entry::getKey, e -> Math.round(e.getValue() * 10.0) / 10.0)));
+
         }
     }
 
